6c9711cab13e120ae340167c8147903350e1c938
Yinghui Wei <91749253+yinghuiwei@users.noreply.github.com>
2023-03-16 13:15:06 +0000

    minor

--- simulation_1_cox_underlying_copula/table/sim1_table.R
+++ simulation_1_cox_underlying_copula/table/sim1_table.R
@@ -37,7 +37,7 @@ cox_gumbel <- read.csv(file=paste0(dir_results,"simulation1/s1_cox_model_summary
 names(cox_gumbel) <- c("items", "gumbel_bias", "gumbel_cp", "gumbel_mse")
 cox_gumbel <- cox_gumbel %>% select(!contains("item", ignore.case = TRUE))
 
-df <- cbind(cox_normal, cox_gumbel, cox_frank, cox_gumbel)
+df <- cbind(cox_normal, cox_clayton, cox_frank, cox_gumbel)
 
 df <- function_latex_table(df)
 
@@ -49,27 +49,29 @@ df
 row.number <-c(1,3,2,4,6,5)
 df <- cbind(row.number,df)
 df <- df[order(row.number),]
-df <- df %>% select(!row.number)
+df <- df %>% dplyr::select(!row.number)
 df
+
+write.csv(df, file=paste0(dir_results, "sim1_table_cox.csv"))
 
 ###############################################################################################
 # Simulation 1 Model 1
 ###############################################################################################
-model1_normal <- read.csv(file=paste0(dir_results,"s1_model1_summary_normal_exponential.csv" ))
+model1_normal <- read.csv(file=paste0(dir_results,"simulation1/s1_model1_summary_normal_exponential.csv" ))
 names(model1_normal) <- c("items", "normal_bias", "normal_cp", "normal_mse", "normal_run_time")
 model1_normal <- model1_normal %>% select(!contains("time", ignore.case = TRUE))
 
-model1_clayton <- read.csv(file=paste0(dir_results,"s1_model1_summary_clayton_exponential.csv" ))
+model1_clayton <- read.csv(file=paste0(dir_results,"simulation1/s1_model1_summary_clayton_exponential.csv" ))
 index <- which(model1_clayton == "b0")
 model1_clayton <- model1_clayton[-index,]
 names(model1_clayton) <- c("items", "clayton_bias", "clayton_cp", "clayton_mse")
 model1_clayton <- model1_clayton %>% select(!contains(c("item","time"), ignore.case = TRUE))
 
-model1_frank <- read.csv(file=paste0(dir_results,"s1_model1_summary_frank_exponential.csv" ))
+model1_frank <- read.csv(file=paste0(dir_results,"simulation1/s1_model1_summary_frank_exponential.csv" ))
 names(model1_frank) <- c("items", "frank_bias", "frank_cp", "frank_mse", "frank_run_time")
 model1_frank <- model1_frank %>% select(!contains(c("item","time"), ignore.case = TRUE))
 
-model1_gumbel <- read.csv(file=paste0(dir_results,"s1_model1_summary_gumbel_exponential.csv" ))
+model1_gumbel <- read.csv(file=paste0(dir_results,"simulation1/s1_model1_summary_gumbel_exponential.csv" ))
 names(model1_gumbel) <- c("items", "gumbel_bias", "gumbel_cp", "gumbel_mse", "gumbel_run_time")
 model1_gumbel <- model1_gumbel %>% select(!contains(c("item","time"), ignore.case = TRUE))
 
