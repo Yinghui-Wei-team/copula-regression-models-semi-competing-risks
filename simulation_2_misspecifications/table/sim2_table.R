f48efb1255913806d1e0fe2eecff819f850ba3a9
Yinghui Wei <91749253+yinghuiwei@users.noreply.github.com>
2023-03-22 10:49:39 +0000

    refine code for combining simulation 2 results

--- simulation_2_misspecifications/table/sim2_table.R
+++ simulation_2_misspecifications/table/sim2_table.R
@@ -9,6 +9,29 @@ library(dplyr); library(xtable); library(stringr)
 # directory if on own PC
 dir_results <- "../../results/simulation_results/"
 
+# specify a data frame
+df_setup <- function(df){
+  df <- data.frame(matrix(nrow=3,ncol=16))
+  names(df) <- c("model","hr_nt_bias",  "hr_nt_cp", "hr_nt_mse",
+                 "hr_t_bias",   "hr_nt_cp", "hr_nt_mse", 
+                 "ref_rho_bias", "ref_rho_cp", "ref_rho_mse",
+                 "cov_rho_bias", "cov_rho_cp", "cov_rho_mse",
+                 "percent_exp", "percent_weibull", "percent_gompertz")
+  df$model <- c("Exponential", "Weibull", "Gompertz")
+  df
+}
+
+# specify the data in the data frame
+df_input <- function(df, row_ref, results){
+  df[row_ref,2:4] <- results[1,2:4] 
+  df[row_ref,5:7] <- results[2,2:4]
+  df[row_ref,8:10] <- results[3,2:4]
+  df[row_ref,11:13] <- results[4,2:4]
+  df[row_ref,14:16] <- results$percentage_chosen[1:3]
+  df
+}
+
+# convert the data frame into a LaTEX table
 function_latex_table <- function(df){
   for(j in 1:ncol(df)){
     if(j!=ncol(df)){
@@ -30,29 +53,14 @@ frank_exp <- read.csv(file=paste0(dir_results,"simulation2/frank/S2_aic_frank_ex
 frank_weibull <- read.csv(file=paste0(dir_results,"simulation2/frank/S2_aic_frank_weibull_summary.csv"))
 frank_gompertz <- read.csv(file=paste0(dir_results,"simulation2/frank/S2_aic_frank_gompertz_summary.csv"))
 
-df <- data.frame(matrix(nrow=3,ncol=16))
-names(df) <- c("model","hr_nt_bias",  "hr_nt_cp", "hr_nt_mse",
-              "hr_t_bias",   "hr_nt_cp", "hr_nt_mse", 
-              "ref_rho_bias", "ref_rho_cp", "ref_rho_mse",
-              "cov_rho_bias", "cov_rho_cp", "cov_rho_mse",
-              "percent_exp", "percent_weibull", "percent_gompertz")
-
-df$model <- c("Exponential", "Weibull", "Gompertz")
-# exponential
-df[1,2:4] <- frank_exp[1,2:4] 
-df[1,5:7] <- frank_exp[2,2:4]
-df[1,8:10] <- frank_exp[3,2:4]
-df[1,11:13] <- frank_exp[4,2:4]
-df[1,14:16] <- frank_exp$percentage_chosen[1:3]
-
-df_input <- function(df, row_ref, results){
-  df[row_ref,2:4] <- results[1,2:4] 
-  df[row_ref,5:7] <- results[2,2:4]
-  df[row_ref,8:10] <- results[3,2:4]
-  df[row_ref,11:13] <- results[4,2:4]
-  df[row_ref,14:16] <- results$percentage_chosen[1:3]
-  df
-}
+df <- NULL
+df <- df_setup(df)
+# # exponential
+# df[1,2:4] <- frank_exp[1,2:4] 
+# df[1,5:7] <- frank_exp[2,2:4]
+# df[1,8:10] <- frank_exp[3,2:4]
+# df[1,11:13] <- frank_exp[4,2:4]
+# df[1,14:16] <- frank_exp$percentage_chosen[1:3]
 
 df <- df_input(df, row_ref=1, results=frank_exp)
 df <- df_input(df, row_ref=2, results=frank_weibull)
@@ -69,14 +77,8 @@ gumbel_exp <- read.csv(file=paste0(dir_results,"simulation2/gumbel/S2_aic_gumbel
 gumbel_weibull <- read.csv(file=paste0(dir_results,"simulation2/gumbel/S2_aic_gumbel_weibull_summary.csv"))
 gumbel_gompertz <- read.csv(file=paste0(dir_results,"simulation2/gumbel/S2_aic_gumbel_gompertz_summary.csv"))
 
-df <- data.frame(matrix(nrow=3,ncol=16))
-names(df) <- c("model","hr_nt_bias",  "hr_nt_cp", "hr_nt_mse",
-               "hr_t_bias",   "hr_nt_cp", "hr_nt_mse", 
-               "ref_rho_bias", "ref_rho_cp", "ref_rho_mse",
-               "cov_rho_bias", "cov_rho_cp", "cov_rho_mse",
-               "percent_exp", "percent_weibull", "percent_gompertz")
-
-df$model <- c("Exponential", "Weibull", "Gompertz")
+df <- NULL
+df <- df_setup(df)
 
 df <- df_input(df, row_ref=1, results=gumbel_exp)
 df <- df_input(df, row_ref=2, results=gumbel_weibull)
